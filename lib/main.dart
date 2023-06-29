4ee7d4540eca13566a3d68193e7d704b8eeafd65
유병우 <milkbottle@yubyeong-uui-MacBookAir.local>
2023-06-29 17:46:55 +0900

    Chore: Update android sdk version

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:leporemart/src/app.dart';
 import 'package:leporemart/src/configs/amplitude_config.dart';
 import 'package:leporemart/src/controllers/bottom_navigationbar_contoller.dart';
 
-void main() {
+Future<void> main() async {
   Get.put(BottomNavigationbarController());
   WidgetsFlutterBinding.ensureInitialized();
   AmplitudeConfig().init();
   AmplitudeConfig.analytics.logEvent("Main Run");
-  runApp(const MyApp());
+
+  // Sentry + GlitchTip
+  // kDebugMode는 개발모드일때 true, 배포모드일때 false
+  if (kDebugMode == true) {
+    await dotenv.load(fileName: 'assets/config/.env');
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = dotenv.get('GLITCHTIP_DSN');
+        options.attachStacktrace = true;
+      },
+      appRunner: () => runApp(MyApp()),
+    );
+  } else {
+    runApp(MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
