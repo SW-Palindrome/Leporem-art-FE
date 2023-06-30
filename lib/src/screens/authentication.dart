d5dc6206f1f018a4d50d6905386584c4bea4db69
Byeongwoo Yu <bigtog0602@gmail.com>
2023-07-01 00:05:02 +0900

    [PA-45] Firebase Analytics 연동 (#5)
    * Chore: Apply  Firebase Analytics with sample code
    
    * Chore: Apply Firebase Analytics on iOS
    
    ---------
    
    Co-authored-by: 유병우 <milkbottle@yubyeong-uui-MacBookAir.local>

---
+++ lib/src/screens/authentication.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:leporemart/main.dart';
+import 'package:leporemart/src/app.dart';
+import 'package:leporemart/src/configs/amplitude_config.dart';
+
+class Authentication extends StatelessWidget {
+  const Authentication({super.key});
+
+  void _logEvent(String eventName) async {
+    await AmplitudeConfig.analytics.logEvent("Login");
+    await MyApp.analytics.logLogin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회원가입 및 로그인'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                _logEvent('비회원 로그인');
+                Get.to(App());
+              },
+              child: Text('비회원 로그인'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _logEvent('카카오 회원가입 및 로그인');
+                Get.to(App());
+              },
+              child: Text('카카오 회원가입 및 로그인'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _logEvent('구글 회원가입 및 로그인');
+                Get.to(App());
+              },
+              child: Text('구글 회원가입 및 로그인'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _logEvent('애플 회원가입 및 로그인');
+                Get.to(App());
+              },
+              child: Text('애플 회원가입 및 로그인'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
