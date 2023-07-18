47c10a62e3643fe246d83d8bcc011ee99e1d651f
milkbottle0305 <bigtog0602@gmail.com>
2023-07-18 20:45:12 +0900

    Design: search filter

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:leporemart/src/models/item.dart';
 import 'package:leporemart/src/repositories/home_repository.dart';
@@ -5,7 +6,50 @@ import 'package:leporemart/src/repositories/home_repository.dart';
 class HomeController extends GetxController {
   final HomeRepository _homeRepository = HomeRepository();
   RxList<Item> items = <Item>[].obs;
-
+  Rx<int> selectedSearchType = 0.obs; //0 정렬, 1 작품 종류, 2 가격대
+  Rx<int> selectedSortType = 0.obs; //0 최신순, 1 인기순, 2 가격 낮은 순, 3 가격 높은 순
+  Rx<int> selectCategoryType =
+      0.obs; //0 머그컵, 1 술잔, 2 화병, 3 오브제, 4 그릇, 5 기타, -1 아무것도 없을e
+  Rx<RangeValues> selectedPriceRange = RangeValues(0, 36).obs;
+  List<int> priceRange = [
+    1000,
+    2000,
+    3000,
+    4000,
+    5000,
+    6000,
+    7000,
+    8000,
+    9000,
+    10000,
+    15000,
+    20000,
+    25000,
+    30000,
+    35000,
+    40000,
+    45000,
+    50000,
+    55000,
+    60000,
+    65000,
+    70000,
+    75000,
+    80000,
+    85000,
+    90000,
+    95000,
+    100000,
+    200000,
+    300000,
+    400000,
+    500000,
+    600000,
+    700000,
+    800000,
+    900000,
+    1000000
+  ];
   @override
   void onInit() async {
     super.onInit();
@@ -20,5 +64,33 @@ class HomeController extends GetxController {
       // 에러 처리
       print('Error fetching items in controller: $e');
     }
+  }
+
+  void changeSelectedSearchType(int index) {
+    selectedSearchType.value = index;
+  }
+
+  void changeSelectedSortType(int index) {
+    selectedSortType.value = index;
+  }
+
+  void changeSelectedCategoryType(int index) {
+    selectCategoryType.value = index;
+  }
+
+  void changeSelectedPriceRange(RangeValues range) {
+    selectedPriceRange.value = range;
+  }
+
+  void resetSelected() {
+    selectedSortType.value = 0;
+    selectCategoryType.value = 0;
+    selectedPriceRange.value = RangeValues(0, 36);
+  }
+
+  bool isResetValid() {
+    return selectedSortType.value != 0 ||
+        selectCategoryType.value != 0 ||
+        selectedPriceRange.value != RangeValues(0, 36);
   }
 }
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:leporemart/src/controllers/home_controller.dart';
 import 'package:leporemart/src/models/item.dart';
 import 'package:leporemart/src/screens/item_detail_screen.dart';
 import 'package:leporemart/src/theme/app_theme.dart';
+import 'package:leporemart/src/widgets/next_button.dart';
 
 class HomeScreen extends GetView<HomeController> {
   HomeScreen({super.key});
@@ -28,17 +29,15 @@ class HomeScreen extends GetView<HomeController> {
           ),
           SizedBox(height: 20),
           Expanded(
-            child: Obx(
-              () => GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 15,
-                  childAspectRatio: 0.6,
-                ),
-                itemCount: controller.items.length,
-                itemBuilder: (context, index) =>
-                    _itemWidget(controller.items[index]),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 15,
+                childAspectRatio: 0.6,
               ),
+              itemCount: controller.items.length,
+              itemBuilder: (context, index) =>
+                  _itemWidget(controller.items[index]),
             ),
           ),
         ],
@@ -144,25 +143,394 @@ class HomeScreen extends GetView<HomeController> {
     );
   }
 
-  Container _filterDropDown(String text) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Color(0xffe6e7ec)),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          Text(text, style: TextStyle(fontSize: 12)),
-          SizedBox(width: 5),
-          SvgPicture.asset(
-            'assets/icons/arrow_down.svg',
-            height: 10,
-            width: 10,
-            colorFilter: ColorFilter.mode(ColorPalette.grey_4, BlendMode.srcIn),
+  GestureDetector _filterDropDown(String text) {
+    return GestureDetector(
+      onTap: () {
+        switch (text) {
+          case '최신순':
+            controller.changeSelectedSearchType(0);
+            break;
+          case '작품 종류':
+            controller.changeSelectedSearchType(1);
+            break;
+          case '가격대':
+            controller.changeSelectedSearchType(2);
+            break;
+        }
+        Get.bottomSheet(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Obx(
+              () => Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () => controller.changeSelectedSearchType(0),
+                          child: Text(
+                            '정렬',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: controller.selectedSearchType.value == 0
+                                  ? ColorPalette.black
+                                  : ColorPalette.grey_4,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        GestureDetector(
+                          onTap: () => controller.changeSelectedSearchType(1),
+                          child: Text(
+                            '작품 종류',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: controller.selectedSearchType.value == 1
+                                  ? ColorPalette.black
+                                  : ColorPalette.grey_4,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        GestureDetector(
+                          onTap: () => controller.changeSelectedSearchType(2),
+                          child: Text(
+                            '가격대',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: controller.selectedSearchType.value == 2
+                                  ? ColorPalette.black
+                                  : ColorPalette.grey_4,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                          onTap: () {
+                            Get.back();
+                          },
+                          child: SvgPicture.asset(
+                            'assets/icons/cancle.svg',
+                            height: 24,
+                            width: 24,
+                            colorFilter: ColorFilter.mode(
+                                ColorPalette.black, BlendMode.srcIn),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: Get.width * 0.1),
+                    Obx(
+                      () => IndexedStack(
+                          index: controller.selectedSearchType.value,
+                          children: [
+                            _sortModal(),
+                            _categoryModal(),
+                            _priceModal(),
+                          ]),
+                    ),
+                    SizedBox(height: Get.width * 0.1),
+                    _searchModalBottom(),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ],
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(30.0),
+            ),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Color(0xffe6e7ec)),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            Text(text, style: TextStyle(fontSize: 12)),
+            SizedBox(width: 5),
+            SvgPicture.asset(
+              'assets/icons/arrow_down.svg',
+              height: 10,
+              width: 10,
+              colorFilter:
+                  ColorFilter.mode(ColorPalette.grey_4, BlendMode.srcIn),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Column _sortModal() {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () => controller.changeSelectedSortType(0),
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Row(
+              children: [
+                Text(
+                  '최신순',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: controller.selectedSortType.value == 0
+                        ? ColorPalette.purple
+                        : ColorPalette.black,
+                    fontSize: 14,
+                  ),
+                ),
+                Spacer(),
+                controller.selectedSortType.value == 0
+                    ? SvgPicture.asset(
+                        'assets/icons/check.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                            ColorPalette.purple, BlendMode.srcIn),
+                      )
+                    : SizedBox(),
+              ],
+            ),
+          ),
+        ),
+        Divider(color: ColorPalette.grey_2),
+        GestureDetector(
+          onTap: () => controller.changeSelectedSortType(1),
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Row(
+              children: [
+                Text(
+                  '인기순',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: controller.selectedSortType.value == 1
+                        ? ColorPalette.purple
+                        : ColorPalette.black,
+                    fontSize: 14,
+                  ),
+                ),
+                Spacer(),
+                controller.selectedSortType.value == 1
+                    ? SvgPicture.asset(
+                        'assets/icons/check.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                            ColorPalette.purple, BlendMode.srcIn),
+                      )
+                    : SizedBox(),
+              ],
+            ),
+          ),
+        ),
+        Divider(color: ColorPalette.grey_2),
+        GestureDetector(
+          onTap: () => controller.changeSelectedSortType(2),
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Row(
+              children: [
+                Text(
+                  '가격 낮은 순',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: controller.selectedSortType.value == 2
+                        ? ColorPalette.purple
+                        : ColorPalette.black,
+                    fontSize: 14,
+                  ),
+                ),
+                Spacer(),
+                controller.selectedSortType.value == 2
+                    ? SvgPicture.asset(
+                        'assets/icons/check.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                            ColorPalette.purple, BlendMode.srcIn),
+                      )
+                    : SizedBox(),
+              ],
+            ),
+          ),
+        ),
+        Divider(color: ColorPalette.grey_2),
+        GestureDetector(
+          onTap: () => controller.changeSelectedSortType(3),
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Row(
+              children: [
+                Text(
+                  '가격 높은 순',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: controller.selectedSortType.value == 3
+                        ? ColorPalette.purple
+                        : ColorPalette.black,
+                    fontSize: 14,
+                  ),
+                ),
+                Spacer(),
+                controller.selectedSortType.value == 3
+                    ? SvgPicture.asset(
+                        'assets/icons/check.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                            ColorPalette.purple, BlendMode.srcIn),
+                      )
+                    : SizedBox(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  _categoryModal() {
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
+      children: [
+        for (int i = 0; i < 5; i++)
+          GestureDetector(
+            onTap: () => controller.changeSelectedCategoryType(i),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: controller.selectCategoryType.value == i
+                    ? ColorPalette.purple
+                    : Colors.white,
+                border: Border.all(
+                    color: controller.selectCategoryType.value == i
+                        ? ColorPalette.purple
+                        : ColorPalette.grey_3),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Text(
+                '머그컵',
+                style: TextStyle(
+                  color: controller.selectCategoryType.value == i
+                      ? Colors.white
+                      : ColorPalette.black,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          )
+      ],
+    );
+  }
+
+  Row _searchModalBottom() {
+    return Row(
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              controller.resetSelected();
+            },
+            child: Container(
+              height: Get.height * 0.06,
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: controller.isResetValid()
+                        ? ColorPalette.grey_7
+                        : ColorPalette.grey_3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/refresh.svg',
+                      height: 20,
+                      width: 20,
+                      colorFilter: ColorFilter.mode(
+                          controller.isResetValid()
+                              ? ColorPalette.grey_7
+                              : ColorPalette.grey_4,
+                          BlendMode.srcIn),
+                    ),
+                    SizedBox(width: 3),
+                    Text(
+                      '초기화',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: controller.isResetValid()
+                            ? ColorPalette.grey_7
+                            : ColorPalette.grey_4,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 10),
+        NextButton(
+          text: '적용하기',
+          value: controller.isResetValid(),
+          onTap: () => Get.back(),
+          width: Get.width * 0.5,
+        ),
+      ],
+    );
+  }
+
+  _priceModal() {
+    return Column(
+      children: [
+        Text(
+          '${controller.priceRange[controller.selectedPriceRange.value.start.toInt()]}원 ~ ${controller.priceRange[controller.selectedPriceRange.value.end.toInt()]}원',
+          style: TextStyle(
+            color: ColorPalette.black,
+            fontWeight: FontWeight.w600,
+            fontFamily: "PretendardVariable",
+            fontStyle: FontStyle.normal,
+            fontSize: 18.0,
+          ),
+        ),
+        RangeSlider(
+          values: controller.selectedPriceRange.value,
+          min: 0,
+          max: controller.priceRange.length - 1,
+          divisions: controller.priceRange.length - 1,
+          onChanged: (RangeValues newRange) {
+            controller.changeSelectedPriceRange(newRange);
+          },
+          activeColor: ColorPalette.purple,
+          inactiveColor: ColorPalette.grey_3,
+          labels: RangeLabels(
+            controller
+                .priceRange[controller.selectedPriceRange.value.start.toInt()]
+                .toString(),
+            controller
+                .priceRange[controller.selectedPriceRange.value.end.toInt()]
+                .toString(),
+          ),
+        ),
+      ],
     );
   }
 }
