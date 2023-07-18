cbc4d7c17b0a8220dad476ea2d920151401e53b9
milkbottle0305 <bigtog0602@gmail.com>
2023-07-18 23:54:12 +0900

    Rename: move firebase_config.dart

--- lib/src/configs/firebase_config.dart
+++ lib/src/configs/firebase_config.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:leporemart/firebase_options.dart';
+import 'package:leporemart/src/configs/firebase_options.dart';
 
 class FirebaseConfig {
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
