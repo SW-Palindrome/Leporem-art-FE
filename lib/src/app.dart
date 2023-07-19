0dc41a2d30f85da35a023afc0c7c011ea8b93e22
milkbottle0305 <bigtog0602@gmail.com>
2023-07-19 13:19:46 +0900

    Refactor: refactor appbar, scaffold

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,7 +4,6 @@ import 'package:leporemart/src/controllers/bottom_navigationbar_contoller.dart';
 import 'package:leporemart/src/screens/buyer/auction_screen.dart';
 import 'package:leporemart/src/screens/buyer/message_screen.dart';
 import 'package:leporemart/src/screens/buyer/home_screen.dart';
-import 'package:leporemart/src/screens/buyer/item_search_screen.dart';
 import 'package:leporemart/src/screens/buyer/profile_screen.dart';
 import 'package:leporemart/src/screens/buyer/flop_screen.dart';
 import 'package:leporemart/src/widgets/my_app_bar.dart';
@@ -15,51 +14,61 @@ class BuyerApp extends GetView<BottomNavigationbarController> {
 
   @override
   Widget build(BuildContext context) {
+    return Obx(() {
+      switch (controller.selectedIndex.value) {
+        case 0:
+          return _homeScaffold();
+        case 1:
+          return _auctionScaffold();
+        case 2:
+          return _messageScaffold();
+        case 3:
+          return _flopScaffold();
+        case 4:
+          return _profileScaffold();
+        default:
+          return _homeScaffold();
+      }
+    });
+  }
+
+  Scaffold _homeScaffold() {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Obx(() {
-              final selectedIndex = controller.selectedIndex.value;
-              switch (selectedIndex) {
-                case 0:
-                  return MyAppBar(
-                      appBarType: AppBarType.buyerMainPageAppBar,
-                      onTapFirstActionIcon: () => Get.toNamed('/buyer/search'));
-                case 1:
-                  return MyAppBar(appBarType: AppBarType.none);
-                case 2:
-                  return MyAppBar(appBarType: AppBarType.none);
-                case 3:
-                  return MyAppBar(appBarType: AppBarType.none);
-                case 4:
-                  return MyAppBar(appBarType: AppBarType.none);
-                default:
-                  return SizedBox.shrink();
-              }
-            }),
-            Expanded(
-              child: Obx(() {
-                final selectedIndex = controller.selectedIndex.value;
-                switch (selectedIndex) {
-                  case 0:
-                    return BuyerHomeScreen();
-                  case 1:
-                    return AuctionScreen();
-                  case 2:
-                    return MessageScreen();
-                  case 3:
-                    return FlopScreen();
-                  case 4:
-                    return ProfileScreen();
-                  default:
-                    return SizedBox.shrink();
-                }
-              }),
-            ),
-          ],
-        ),
-      ),
+      appBar: MyAppBar(
+          appBarType: AppBarType.buyerMainPageAppBar,
+          onTapFirstActionIcon: () => Get.toNamed('/buyer/search')),
+      body: BuyerHomeScreen(),
+      bottomNavigationBar: MyBottomNavigationBar(),
+    );
+  }
+
+  Widget _auctionScaffold() {
+    return Scaffold(
+      appBar: MyAppBar(appBarType: AppBarType.none),
+      body: AuctionScreen(),
+      bottomNavigationBar: MyBottomNavigationBar(),
+    );
+  }
+
+  Widget _messageScaffold() {
+    return Scaffold(
+      appBar: MyAppBar(appBarType: AppBarType.none),
+      body: MessageScreen(),
+      bottomNavigationBar: MyBottomNavigationBar(),
+    );
+  }
+
+  Widget _flopScaffold() {
+    return Scaffold(
+      appBar: MyAppBar(appBarType: AppBarType.none),
+      body: FlopScreen(),
+      bottomNavigationBar: MyBottomNavigationBar(),
+    );
+  }
+
+  Widget _profileScaffold() {
+    return Scaffold(
+      body: SafeArea(child: ProfileScreen()),
       bottomNavigationBar: MyBottomNavigationBar(),
     );
   }
