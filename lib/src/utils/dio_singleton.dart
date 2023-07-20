885a7c43b668eebac749df397bd31fe20bc9ce61
milkbottle0305 <bigtog0602@gmail.com>
2023-07-20 14:12:38 +0900

    Feat: add interceptor on api request for refresh token if 401

--- lib/src/utils/dio_singleton.dart
+++ lib/src/utils/dio_singleton.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:leporemart/src/configs/login_config.dart';
 
 class DioSingleton {
   static Dio? _dioInstance;
@@ -10,6 +11,26 @@ class DioSingleton {
       _dioInstance!.options.validateStatus = (status) {
         return status! < 500;
       };
+      _dioInstance!.interceptors.add(InterceptorsWrapper(
+        onRequest: (request, handler) async {
+          final idToken = await getOAuthToken().then((value) => value!.idToken);
+          request.headers['Authorization'] = 'Palindrome $idToken';
+          return handler.next(request);
+        },
+        onResponse: (response, handler) async {
+          if (response.statusCode == 401) {
+            await refreshIDToken();
+            final idToken =
+                await getOAuthToken().then((value) => value!.idToken);
+            response.requestOptions.headers['Authorization'] =
+                'Palindrome $idToken';
+            return handler.next(response);
+          } else {
+            return handler.next(response);
+          }
+        },
+        onError: (error, handler) async {},
+      ));
     }
     return _dioInstance!;
   }
