b6196ee5cb71a5d01d5f9d49e972d6c50c9d361f
milkbottle0305 <bigtog0602@gmail.com>
2023-07-22 23:36:57 +0900

    Feat: change only index to buyer seller index

--- lib/src/controllers/bottom_navigationbar_contoller.dart
+++ lib/src/controllers/bottom_navigationbar_contoller.dart
@@ -7,11 +7,12 @@ class MyBottomNavigationbarController extends GetxController {
   static MyBottomNavigationbarController get to => Get.find();
 
   // 현재 선택된 탭 아이템 번호 저장
-  final RxInt selectedIndex = 0.obs;
+  final Rx<int> selectedBuyerIndex = 0.obs;
+  final Rx<int> selectedSellerIndex = 0.obs;
 
   // 탭 이벤트가 발생할 시 selectedIndex값을 변경해줄 함수
-  void changeIndex(int index) {
-    selectedIndex(index);
+  void changeBuyerIndex(int index) {
+    selectedBuyerIndex(index);
     //하단바 선택시 로그
     late String pageStr;
     switch (index) {
@@ -31,6 +32,33 @@ class MyBottomNavigationbarController extends GetxController {
         pageStr = "Mypage";
         break;
     }
+
+    if (!kDebugMode) {
+      AmplitudeConfig.analytics
+          .logEvent("Page View", eventProperties: {"Page Name": pageStr});
+      FirebaseConfig.analytics.logEvent(name: pageStr);
+    }
+  }
+
+  void changeSellerIndex(int index) {
+    selectedSellerIndex(index);
+    //하단바 선택시 로그
+    late String pageStr;
+    switch (index) {
+      case 0:
+        pageStr = "List";
+        break;
+      case 1:
+        pageStr = "Custom Order";
+        break;
+      case 2:
+        pageStr = "Chat";
+        break;
+      case 3:
+        pageStr = "Mypage";
+        break;
+    }
+
     if (!kDebugMode) {
       AmplitudeConfig.analytics
           .logEvent("Page View", eventProperties: {"Page Name": pageStr});
