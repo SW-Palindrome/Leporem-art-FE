92788ebcfccb6094ef1e7c68a41f165340bc533b
jshong0907 <jshong0907@gmail.com>
2023-07-25 15:14:21 +0900

    Feature: get chatroom list controller

---
+++ lib/src/controllers/buyer_message_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+import '../models/message.dart';
+import '../repositories/message_repository.dart';
+
+class BuyerMessageController extends GetxController {
+  final MessageRepository _messageRepository = MessageRepository();
+  RxList<ChatRoom> chatRoomList = <ChatRoom>[].obs;
+  int currentPage = 1;
+
+  @override
+  void onInit() async {
+    super.onInit();
+    await fetch();
+  }
+
+  Future<void> fetch() async {
+    final List<ChatRoom> fetchedChatRoomList = await _messageRepository.fetchChatRooms(currentPage);
+    chatRoomList.addAll(fetchedChatRoomList);
+    currentPage++;
+  }
+}
