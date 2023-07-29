a0cd953fd24e1fdaf67b0f91b7d4cbd74b85c7a8
milkbottle0305 <bigtog0602@gmail.com>
2023-07-30 02:14:18 +0900

    Feat: add like function when buyer main page

--- lib/src/screens/buyer/home_screen.dart
+++ lib/src/screens/buyer/home_screen.dart
@@ -55,7 +55,7 @@ class BuyerHomeScreen extends GetView<BuyerHomeController> {
                   itemCount: controller.items.length,
                   itemBuilder: (context, index) {
                     if (index < controller.items.length) {
-                      return itemWidget(controller.items[index]);
+                      return _itemWidget(index);
                     } else {
                       // Show a loading indicator at the end of the grid
                       return Center(child: CircularProgressIndicator());
@@ -66,6 +66,136 @@ class BuyerHomeScreen extends GetView<BuyerHomeController> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  _itemWidget(int index) {
+    return GestureDetector(
+      onTap: () {
+        Get.to(BuyerItemDetailScreen(),
+            arguments: {'item_id': controller.items[index].id});
+        Get.put(ItemDetailController());
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Stack(
+                children: [
+                  ExtendedImage.network(
+                    controller.items[index].thumbnailUrl,
+                    fit: BoxFit.cover,
+                    width: Get.width * 0.5,
+                    height: Get.width * 0.5,
+                    cache: true,
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    right: 10,
+                    child: controller.items[index].isLiked
+                        ? GestureDetector(
+                            onTap: () async {
+                              await controller
+                                  .unlike(controller.items[index].id);
+                            },
+                            child: SvgPicture.asset(
+                              'assets/icons/heart_fill.svg',
+                              height: 24,
+                              width: 24,
+                              colorFilter: ColorFilter.mode(
+                                  ColorPalette.purple, BlendMode.srcIn),
+                            ),
+                          )
+                        : GestureDetector(
+                            onTap: () async {
+                              await controller.like(controller.items[index].id);
+                            },
+                            child: SvgPicture.asset(
+                              'assets/icons/heart_outline.svg',
+                              height: 24,
+                              width: 24,
+                              colorFilter: ColorFilter.mode(
+                                  ColorPalette.white, BlendMode.srcIn),
+                            ),
+                          ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: ColorPalette.white,
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(10))),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    controller.items[index].creator,
+                    style: TextStyle(
+                      color: ColorPalette.grey_4,
+                      fontSize: 10,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    height: Get.height * 0.04,
+                    child: Text(
+                      controller.items[index].name,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: ColorPalette.black,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    '${controller.items[index].price.toString().toString().replaceAllMapped(
+                          RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+                          (Match m) => '${m[1]},',
+                        )}원',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: ColorPalette.black,
+                      fontSize: 13,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/heart_fill.svg',
+                        height: 12,
+                        width: 12,
+                        colorFilter: ColorFilter.mode(
+                            controller.items[index].likes != 0
+                                ? ColorPalette.purple
+                                : Colors.transparent,
+                            BlendMode.srcIn),
+                      ),
+                      SizedBox(width: 2),
+                      Text(
+                        '${controller.items[index].likes}',
+                        style: TextStyle(
+                          color: controller.items[index].likes != 0
+                              ? ColorPalette.purple
+                              : Colors.transparent,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
