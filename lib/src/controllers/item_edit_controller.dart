0e78d762336214ccb08939df5c91586cf4a5cfa9
milkbottle0305 <bigtog0602@gmail.com>
2023-07-31 17:07:09 +0900

    Feat: apply initial value except images and video

--- lib/src/controllers/item_edit_controller.dart
+++ lib/src/controllers/item_edit_controller.dart
@@ -12,7 +12,40 @@ class ItemEditController extends ItemCreateDetailController {
   void load() {
     titleController.text =
         Get.find<SellerItemDetailController>().itemDetail.value.title;
-    print(
-        'titlecontroller: ${Get.find<SellerItemDetailController>().itemDetail.value.title}');
+    descriptionController.text =
+        Get.find<SellerItemDetailController>().itemDetail.value.description;
+    priceController.text = Get.find<SellerItemDetailController>()
+        .itemDetail
+        .value
+        .price
+        .toString()
+        .replaceAllMapped(
+          RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+          (Match m) => '${m[1]},',
+        );
+    ;
+    widthController.text =
+        Get.find<SellerItemDetailController>().itemDetail.value.width ??
+            ''.toString();
+    depthController.text =
+        Get.find<SellerItemDetailController>().itemDetail.value.depth ??
+            ''.toString();
+    heightController.text =
+        Get.find<SellerItemDetailController>().itemDetail.value.height ??
+            ''.toString();
+    amount.value =
+        Get.find<SellerItemDetailController>().itemDetail.value.currentAmount;
+    List<String> categoryList =
+        Get.find<SellerItemDetailController>().itemDetail.value.category;
+    for (int i = 0; i < categoryList.length; i++) {
+      for (int j = 0; j < categoryTypes.length; j++) {
+        if (categoryList[i] == categoryTypes[j]) {
+          print(
+              'categoryList[i] : categoryList[$i] / categoryTypes[j] : categoryTypes[$j]');
+          selectedCategoryType[j] = true;
+          continue;
+        }
+      }
+    }
   }
 }
