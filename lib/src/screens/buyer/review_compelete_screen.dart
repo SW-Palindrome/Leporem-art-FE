0bad615795b0a1b138b904eba62aa0a251755372
milkbottle0305 <bigtog0602@gmail.com>
2023-08-02 15:34:38 +0900

    Design: create review compelete button

--- lib/src/screens/buyer/review_compelete_screen.dart
+++ lib/src/screens/buyer/review_compelete_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:leporemart/src/screens/buyer/order_list_screen.dart';
 import 'package:leporemart/src/theme/app_theme.dart';
-import 'package:leporemart/src/widgets/my_app_bar.dart';
+import 'package:leporemart/src/widgets/next_button.dart';
 
 class ReviewCompeleteScreen extends StatelessWidget {
   const ReviewCompeleteScreen({super.key});
@@ -10,12 +11,6 @@ class ReviewCompeleteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(
-        appBarType: AppBarType.backAppBar,
-        onTapLeadingIcon: () {
-          Get.back();
-        },
-      ),
       body: SafeArea(
         child: _complete(),
       ),
@@ -23,19 +18,44 @@ class ReviewCompeleteScreen extends StatelessWidget {
   }
 
   _complete() {
-    return Center(
-      child: Column(
-        children: [
-          SvgPicture.asset('assets/icons/complete.svg'),
-          SizedBox(height: 40),
-          Text('후기 작성을 완료했습니다!',
-              style: TextStyle(
-                fontSize: 20,
-                color: ColorPalette.black,
-                fontFamily: FontPalette.pretenderd,
-              )),
-        ],
-      ),
+    return Stack(
+      children: [
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/complete.svg',
+                width: Get.width * 0.22,
+                height: Get.width * 0.22,
+              ),
+              SizedBox(height: 40),
+              Text(
+                '후기 작성을 완료했습니다!',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: ColorPalette.black,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: FontPalette.pretenderd,
+                ),
+              ),
+              SizedBox(height: 100),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: NextButton(
+                text: '주문 내역으로 돌아가기',
+                value: true,
+                onTap: () {
+                  Get.offAll(BuyerOrderListScreen());
+                }),
+          ),
+        )
+      ],
     );
   }
 }
