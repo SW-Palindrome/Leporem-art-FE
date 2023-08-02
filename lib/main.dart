6847479f0c4aebe3412a3d3053fa5ffa34dfa3c3
홍준식 <jshong0907@gmail.com>
2023-08-02 17:04:08 +0900

    Feature: save user global info to controller (#47)

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,13 @@ import 'package:leporemart/src/controllers/account_type_controller.dart';
 import 'package:leporemart/src/controllers/agreement_controller.dart';
 import 'package:leporemart/src/controllers/email_controller.dart';
 import 'package:leporemart/src/controllers/nickname_controller.dart';
+import 'package:leporemart/src/controllers/user_global_info_controller.dart';
 import 'package:leporemart/src/screens/account/login_screen.dart';
 import 'package:leporemart/src/theme/app_theme.dart';
 import 'package:leporemart/src/utils/chatting_socket_singleton.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:leporemart/src/configs/amplitude_config.dart';
 import 'package:leporemart/src/controllers/bottom_navigationbar_contoller.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
-import 'package:socket_io_client/socket_io_client.dart';
 
 void main() async {
   // Sentry + GlitchTip
@@ -31,6 +30,7 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  Get.put(UserGlobalInfoController());
   Get.lazyPut(() => MyBottomNavigationbarController());
   Get.lazyPut(() => AgreementController());
   Get.lazyPut(() => AccountTypeController());
---
+++ lib/src/controllers/user_global_info_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class UserGlobalInfoController extends GetxController {
+  late final int userId;
+}
