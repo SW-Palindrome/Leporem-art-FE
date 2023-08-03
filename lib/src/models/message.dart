5a1a3f82fb9dfb6cf8e958cdf16646fbf2ed2a03
jshong0907 <jshong0907@gmail.com>
2023-08-03 15:33:13 +0900

    Feature: message model

--- lib/src/models/message.dart
+++ lib/src/models/message.dart
@@ -1,12 +1,33 @@
+class Message {
+  String messageId;
+  final int userId;
+  final DateTime writeDatetime;
+  final bool isRead;
+  final String message;
+
+  Message({
+    required this.messageId,
+    required this.userId,
+    required this.writeDatetime,
+    required this.isRead,
+    required this.message,
+  });
+}
+
 class ChatRoom {
   final int chatRoomId;
+  final int opponentUserId;
   final String opponentNickname;
   final String opponentProfileImageUrl;
   final DateTime lastMessageDatetime;
   final String lastMessage;
 
+  List<Message> messageList = <Message>[];
+  List<Message> tempMessageList = <Message>[];
+
   ChatRoom({
     required this.chatRoomId,
+    required this.opponentUserId,
     required this.opponentNickname,
     required this.opponentProfileImageUrl,
     required this.lastMessageDatetime,
@@ -16,6 +37,7 @@ class ChatRoom {
   factory ChatRoom.fromJson(Map<String, dynamic> json) {
     return ChatRoom(
       chatRoomId: json['chat_room_id'],
+      opponentUserId: json['opponent_user_id'],
       opponentNickname: json['opponent_nickname'],
       opponentProfileImageUrl: json['opponent_profile_image'],
       lastMessageDatetime: DateTime.parse(json['last_message_datetime']),
