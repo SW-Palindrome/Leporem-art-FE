838480147b71c45b1c7680047bd5b54c6b315b7b
jshong0907 <jshong0907@gmail.com>
2023-08-03 15:34:03 +0900

    Feature: message register logic

--- lib/src/controllers/buyer_message_controller.dart
+++ lib/src/controllers/buyer_message_controller.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
+import 'package:leporemart/src/controllers/user_global_info_controller.dart';
+import 'package:uuid/uuid.dart';
 
 import '../models/message.dart';
 import '../repositories/message_repository.dart';
+import '../utils/chatting_socket_singleton.dart';
 
 class BuyerMessageController extends GetxController {
   final MessageRepository _messageRepository = MessageRepository();
   RxList<ChatRoom> chatRoomList = <ChatRoom>[].obs;
-  int currentPage = 1;
 
   @override
   void onInit() async {
@@ -17,6 +19,38 @@ class BuyerMessageController extends GetxController {
   Future<void> fetch() async {
     List<ChatRoom> fetchedChatRoomList = await _messageRepository.fetchChatRooms(currentPage);
     chatRoomList.addAll(fetchedChatRoomList);
-    currentPage++;
+  }
+
+  Future<void> sendMessage(chatRoomId, message) async {
+    int opponentUserId = chatRoomList.firstWhere((chatRoom) => chatRoom.chatRoomId == chatRoomId).opponentUserId;
+    ChatRoom chatRoom = chatRoomList.firstWhere((chatRoom) => chatRoom.chatRoomId == chatRoomId);
+    UserGlobalInfoController userGlobalInfoController = Get.find<UserGlobalInfoController>();
+    chatRoom.tempMessageList.add(Message(
+      messageId: Uuid().v4(),
+      userId: userGlobalInfoController.userId,
+      writeDatetime: DateTime.now(),
+      isRead: false,
+      message: message,
+    ));
+    ChattingSocketSingleton().sendMessage(chatRoomId, opponentUserId, message);
+  }
+
+  registerMessage(chatRoomId, messageTempId, messageId) {
+    ChatRoom sendChatRoom = chatRoomList.firstWhere((chatRoom) => chatRoom.chatRoomId == chatRoomId);
+    Message tempMessage = sendChatRoom.tempMessageList.firstWhere((message) => message.messageId == messageTempId);
+    sendChatRoom.tempMessageList.remove(tempMessage);
+    tempMessage.messageId = messageId.toString();
+    sendChatRoom.messageList.add(tempMessage);
+  }
+
+  receiveMessage(chatRoomId, messageId, message) {
+    ChatRoom receiveChatRoom = chatRoomList.firstWhere((chatRoom) => chatRoom.chatRoomId == chatRoomId);
+    receiveChatRoom.messageList.add(Message(
+        messageId: messageId,
+        userId: receiveChatRoom.opponentUserId,
+        writeDatetime: DateTime.now(),
+        isRead: false,
+        message: message,
+    ));
   }
 }
