eab6cd5ee68a8537e912ff88420f2aa3305a2cf7
milkbottle0305 <bigtog0602@gmail.com>
2023-08-16 02:35:20 +0900

    Fix: remove final keyword in user global controller

--- lib/src/controllers/user_global_info_controller.dart
+++ lib/src/controllers/user_global_info_controller.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 
 class UserGlobalInfoController extends GetxController {
-  late final int userId;
+  late int userId;
   late UserType userType;
   late String nickname;
   late bool isSeller;
