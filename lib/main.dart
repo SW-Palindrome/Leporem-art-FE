35d59e81b904e6a02994e42a0384b5b0285834fe
milkbottle0305 <bigtog0602@gmail.com>
2023-09-25 15:53:15 +0900

    Feat: apply notification

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -15,6 +16,8 @@ import 'package:leporemart/src/configs/login_config.dart';
 import 'package:leporemart/src/controllers/user_global_info_controller.dart';
 import 'package:leporemart/src/screens/account/home.dart';
 import 'package:leporemart/src/theme/app_theme.dart';
+import 'package:leporemart/src/utils/chatting_socket_singleton.dart';
+import 'package:leporemart/src/utils/notification.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:leporemart/src/configs/amplitude_config.dart';
 import 'package:leporemart/src/controllers/bottom_navigationbar_contoller.dart';
@@ -40,11 +43,11 @@ void main() async {
   KakaoSdk.init(nativeAppKey: dotenv.get('KAKAO_APIKEY'));
 
   bool isLoginProceed = await getLoginProceed();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  String? _fcmToken = await FirebaseMessaging.instance.getToken();
-  print('fcmToken: $_fcmToken');
+  await Firebase.initializeApp();
+
+  // setting 함수
+  await setupFlutterNotifications();
+
   if (kReleaseMode) {
     await FirebaseConfig.init();
     await AmplitudeConfig.init();
@@ -59,9 +62,25 @@ void main() async {
   runApp(MyApp(isLoginProceed: isLoginProceed));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final bool isLoginProceed;
   MyApp({super.key, required this.isLoginProceed});
+  @override
+  State<MyApp> createState() => _MyAppState(isLoginProceed: isLoginProceed);
+}
+
+class _MyAppState extends State<MyApp> {
+  final bool isLoginProceed;
+  _MyAppState({required this.isLoginProceed});
+
+  @override
+  void initState() {
+    super.initState();
+    // foreground 수신처리
+    FirebaseMessaging.onMessage.listen(showFlutterNotification);
+    // background 수신처리
+    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/src/utils/notification.dart
@@ -0,0 +1,97 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import '../configs/firebase_options.dart';
+
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+late AndroidNotificationChannel channel;
+bool isFlutterLocalNotificationsInitialized = false; // 셋팅여부 판단 flag
+
+/// 셋팅 메소드
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  // iOS foreground notification 권한
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  // IOS background 권한 체킹 , 요청
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  // 토큰 요청
+  getToken();
+  // 셋팅flag 설정
+  isFlutterLocalNotificationsInitialized = true;
+}
+
+Future<void> getToken() async {
+  // ios
+  String? token;
+  if (defaultTargetPlatform == TargetPlatform.iOS ||
+      defaultTargetPlatform == TargetPlatform.macOS) {
+    token = await FirebaseMessaging.instance.getAPNSToken();
+  }
+  // aos
+  else {
+    token = await FirebaseMessaging.instance.getToken();
+  }
+  print("fcmToken : $token");
+}
+
+/// fcm 배경 처리 (종료되어있거나, 백그라운드에 경우)
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await setupFlutterNotifications(); // 셋팅 메소드
+  //showFlutterNotification(message);  // 로컬노티
+}
+
+/// fcm 전경 처리 - 로컬 알림 보이기
+void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null && !kIsWeb) {
+    // 웹이 아니면서 안드로이드이고, 알림이 있는경우
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          // TODO add a proper drawable resource to android, for now using
+          //      one that already exists in example app.
+          icon: '@drawable/ic_stat_notiicon',
+        ),
+      ),
+    );
+  }
+}
