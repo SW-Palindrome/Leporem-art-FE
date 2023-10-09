2468d9226551e51c411afdf6c906fd8ec82073f8
milkbottle0305 <bigtog0602@gmail.com>
2023-10-09 21:42:52 +0900

    Design: create bottomsheet for using template

--- lib/app/ui/app/seller/exhibition_create_seller/widgets/template_select_widget.dart
+++ lib/app/ui/app/seller/exhibition_create_seller/widgets/template_select_widget.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
+import '../../../../../controller/seller/exhibition/exhibition_controller.dart';
 import '../../../../theme/app_theme.dart';
+import '../../../widgets/bottom_sheet.dart';
 
 Widget templateSelectWidget() {
+  final controller = Get.find<ExhibitionController>();
+
   return Container(
     padding: EdgeInsets.all(12),
     decoration: BoxDecoration(
@@ -13,7 +18,42 @@ Widget templateSelectWidget() {
     ),
     child: Row(
       children: [
-        SvgPicture.asset('assets/icons/checkbox_unselect.svg'),
+        Obx(
+          () => GestureDetector(
+              onTap: () {
+                Get.bottomSheet(
+                  MyBottomSheet(
+                    height: Get.height * 0.3,
+                    title: '템플릿을 사용할까요?',
+                    description: '입력했던 정보가 사라집니다.',
+                    buttonType: BottomSheetType.twoButton,
+                    leftButtonText: '취소',
+                    onLeftButtonPressed: () {
+                      Get.back();
+                    },
+                    rightButtonText: '변경',
+                    onRightButtonPressed: () {
+                      controller.sellerInfoReset();
+                      controller.isSellerTemplateUsed.value =
+                          !controller.isSellerTemplateUsed.value;
+                      Get.back();
+                    },
+                    onCloseButtonPressed: () {
+                      Get.back();
+                    },
+                  ),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(30.0),
+                    ),
+                  ),
+                );
+              },
+              child: controller.isSellerTemplateUsed.value == true
+                  ? SvgPicture.asset('assets/icons/checkbox_select.svg')
+                  : SvgPicture.asset('assets/icons/checkbox_unselect.svg')),
+        ),
         SizedBox(width: 12),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
