0ca99ace4e9a34f334c4b2f9b431d0207e42de39
milkbottle0305 <bigtog0602@gmail.com>
2023-10-17 16:30:53 +0900

    Design: create 4th template

--- lib/app/ui/app/seller/exhibition_create_item_example/widgets/exhibition_template_carousel_widget.dart
+++ lib/app/ui/app/seller/exhibition_create_item_example/widgets/exhibition_template_carousel_widget.dart
@@ -41,19 +41,19 @@ exhibitionTemplateCarouselWidget() {
               'https://leporem-art-media-prod.s3.ap-northeast-2.amazonaws.com/items/item_image/e29ad86f-7255-43e9-b596-f59dc4c90957.jpg',
               'https://leporem-art-media-prod.s3.ap-northeast-2.amazonaws.com/items/item_image/e29ad86f-7255-43e9-b596-f59dc4c90957.jpg'
             ],
-            0x0000000,
+            0xffFFEADE,
             FontPalette.chosun,
           ),
-
           _template4Widget(
             '솔방울을 머금은 술잔',
             '추풍낙엽 속 길을 거닐며 떨어진 솔방울을 보고 명감을 받아 만든 술잔입니다.',
             [
               'https://leporem-art-media-prod.s3.ap-northeast-2.amazonaws.com/items/item_image/e29ad86f-7255-43e9-b596-f59dc4c90957.jpg',
               'https://leporem-art-media-prod.s3.ap-northeast-2.amazonaws.com/items/item_image/e29ad86f-7255-43e9-b596-f59dc4c90957.jpg',
+              'https://leporem-art-media-prod.s3.ap-northeast-2.amazonaws.com/items/item_image/e29ad86f-7255-43e9-b596-f59dc4c90957.jpg',
               'https://leporem-art-media-prod.s3.ap-northeast-2.amazonaws.com/items/item_image/e29ad86f-7255-43e9-b596-f59dc4c90957.jpg'
             ],
-            0x0000000,
+            0xffFFEADE,
             FontPalette.chosun,
           ),
         ],
@@ -325,61 +325,77 @@ _template4Widget(String title, String description, List<String> imageUrlList,
     int color, String fontFamily) {
   return Container(
     width: Get.width,
-    height: Get.width,
+    height: Get.width * 1.1,
     color: Color(color),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        CarouselSlider(
-          items: [
-            for (final imageUrl in imageUrlList)
-              Padding(
-                padding: const EdgeInsets.only(left: 8, top: 8),
-                child: CachedNetworkImage(
-                  imageUrl: imageUrl,
-                  width: Get.width,
-                  height: Get.width * 0.8,
-                  fit: BoxFit.cover,
-                ),
-              ),
-          ],
-          options: CarouselOptions(
-            aspectRatio: 1 / 0.75,
-            viewportFraction: 0.75,
-            enableInfiniteScroll: false,
-            padEnds: false,
-          ),
-        ),
-        SizedBox(height: 12),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Text(
-              title,
-              style: TextStyle(
-                color: ColorPalette.black,
-                fontFamily: fontFamily,
-                fontWeight: FontWeight.bold,
-                fontSize: 26,
-              ),
+    child: Padding(
+      padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color:
+                  color == 0xff000000 ? ColorPalette.white : ColorPalette.black,
+              fontWeight: FontWeight.w600,
+              fontFamily: fontFamily,
+              fontSize: 26,
             ),
           ),
-        ),
-        SizedBox(height: 12),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Text(
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  SizedBox(height: Get.width * 0.112),
+                  CachedNetworkImage(
+                    imageUrl: imageUrlList[0],
+                    width: Get.width * 0.4466,
+                    height: Get.width * 0.32,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 8),
+                  CachedNetworkImage(
+                    imageUrl: imageUrlList[1],
+                    width: Get.width * 0.4466,
+                    height: Get.width * 0.32,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+              SizedBox(width: 8),
+              Column(
+                children: [
+                  SizedBox(height: Get.width * 0.016),
+                  CachedNetworkImage(
+                    imageUrl: imageUrlList[2],
+                    width: Get.width * 0.4466,
+                    height: Get.width * 0.32,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 8),
+                  CachedNetworkImage(
+                    imageUrl: imageUrlList[3],
+                    width: Get.width * 0.4466,
+                    height: Get.width * 0.32,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Text(
             description,
             style: TextStyle(
-              color: ColorPalette.black,
+              color:
+                  color == 0xff000000 ? ColorPalette.white : ColorPalette.black,
               fontFamily: fontFamily,
               fontSize: 14,
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   );
 }
