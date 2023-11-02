0493c7a76303cb3a07d0a017a581b9a9918fbf61
milkbottle0305 <bigtog0602@gmail.com>
2023-11-02 15:57:02 +0900

    Design: 기간에 스페이스바 하나 더 있던 것 제거

--- lib/app/ui/app/seller/exhibition_create_start/widgets/exhibition_period_widget.dart
+++ lib/app/ui/app/seller/exhibition_create_start/widgets/exhibition_period_widget.dart
@@ -26,7 +26,7 @@ exhibitionPeriodWidget(String startDate, String endDate) {
         SizedBox(height: 8),
         Text(
           //2023-10-26T11:00:00Z -> 2023년 10월 26일 오후 12:00로 바꾸는 로직
-          '${startDate.substring(0, 4)}년 ${startDate.substring(5, 7)}월 ${startDate.substring(8, 10)}일 ${startDate.substring(11, 16)} ~ ${endDate.substring(0, 4)}년 ${endDate.substring(5, 7)}월 ${endDate.substring(8, 10)}일  ${endDate.substring(11, 16)}',
+          '${startDate.substring(0, 4)}년 ${startDate.substring(5, 7)}월 ${startDate.substring(8, 10)}일 ${startDate.substring(11, 16)} ~ ${endDate.substring(0, 4)}년 ${endDate.substring(5, 7)}월 ${endDate.substring(8, 10)}일 ${endDate.substring(11, 16)}',
           style: TextStyle(
             color: ColorPalette.black,
             fontFamily: FontPalette.pretendard,
